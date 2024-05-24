da2f1b58c30c8058db609fa3839376af0bb51155
gabbytb <igabriel.oyebanji@travelbeta.com>
2024-05-24 09:21:55 +0100

    Pushed to Repository 24/05/2024

--- src/sections/HomeSectionSeven.jsx
+++ src/sections/HomeSectionSeven.jsx
@@ -6,50 +6,59 @@ import { Disqus } from "../components";
 
 
 
-
-
-
 const HomeSectionSeven = () => {
-
     
-    // console.clear();
+    
+    // Managing the active state of The INDEXED Flip Items
+    const [itemToFlip, setItemToFlip] = useState(flipItems);
+    // Managing the active state of The INDEXED Flip Items
 
 
+    // Value changes to represent the "Active Index" of The INDEXED Flip Items
     const [count, setCount] = useState(0);
     console.log('Count: ', count);
+    // Value changes to represent the "Active Index" of The INDEXED Flip Items
 
-    const [itemToFlip, setItemToFlip] = useState(flipItems);
+
 
 
 
 
     useEffect(() => {
-        setInterval(() => {
+        const timer = setInterval(() => {
             console.clear();
+
 
             function autoIncrement() {  
                 for (var n = 0; n < itemToFlip.length; n++) {                                       
                     if (n < itemToFlip.length) {
+
+
                         var disqusWrapID = document.getElementById('disqusWrapId');
                         var disqusObj = disqusWrapID.getElementsByClassName('disqus');
-                                              
-                        
-                        for (var i = 0; i < disqusObj.length; n++) {
-                            if ((count < n) && (count < disqusObj.length)) {
-                                setCount(count+1);
+
+
+                        for (var i = 0; i < disqusObj.length; i++) {
+                            console.log('Active Number: ', n);
+                            console.log('Flip Object: ', itemToFlip[n]);
+
+                            console.log('Current Number: ', i);
+                            console.log('Discuss Object: ', disqusObj[i]);
+
+
+                            // setCount(prevCount => (prevCount < i ? prevCount + 1 : 0));
                                 
+                            // if (n !== i) {
+                            //     disqusObj[i]?.classList.add('is-active');
+                            //     disqusObj[i]?.classList.remove('hidden');
+                            // } else {
+                            //     disqusObj[i]?.classList.remove('is-active');
+                            //     disqusObj[i]?.classList.add('hidden');  
+                            // };
 
-                                if (n !== i) {
-                                    disqusObj[i]?.classList.add('is-active');
-                                    disqusObj[i]?.classList.remove('hidden');
-                                } else {
-                                    disqusObj[i]?.classList.remove('is-active');
-                                    disqusObj[i]?.classList.add('hidden');  
-                                };
-
-                            } else {
-                                setCount(0);
-                            };
+                            // } else {
+                            //     setCount(0);
+                            // };
                         };
                     };
                 };
@@ -57,6 +66,9 @@ const HomeSectionSeven = () => {
 
             autoIncrement();
         }, 2000);
+
+        return () => clearInterval(timer);
+
     }, [count, itemToFlip]);
 
     
@@ -84,7 +96,7 @@ const HomeSectionSeven = () => {
         //     // } else {
         //     //     return console.log('False: ', false);
         //     // }
-        // }
+    // }
 
 
     
