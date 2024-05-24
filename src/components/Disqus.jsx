208beffdf9b1d89b6d847035d4208bcdb0be90ff
gabbytb <igabriel.oyebanji@travelbeta.com>
2024-05-24 12:17:34 +0100

    Pushed commit to Repository

--- src/components/Disqus.jsx
+++ src/components/Disqus.jsx
@@ -1,7 +1,7 @@
 const Disqus = ({ title, subtitle }) => {
 
   return (
-    <div className="disqus">
+    <div id="disqusID" className="disqus">
         <span className="disqus-q">{title}</span>
         <span className="disqus-a">{subtitle}</span>
     </div>
