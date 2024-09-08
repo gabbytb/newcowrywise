1fff9bb3c9055849f784ac6459ce011efeb84f56
gabbytb <igabriel.oyebanji@travelbeta.com>
2024-09-08 21:51:00 +0100

    Pushed to Repository (vybezonvibes@protonmail.com)

--- src/pages/VerifySignup.jsx
+++ src/pages/VerifySignup.jsx
@@ -162,21 +162,6 @@ const VerifySignUp = () => {
     const [verificationSuccessful, setVerificationSuccessful] = useState(false);
     const [verificationMessage, setVerificationMessage] = useState("");
 
-    // Function to get a cookie by name
-    // function getCookie(name) {
-    //     const nameEQ = name + "=";
-    //     const ca = document.cookie.split(';');
-    //     for (let i = 0; i < ca.length; i++) {
-    //     let c = ca[i];
-    //     while (c.charAt(0) === ' ') c = c.substring(1);
-    //     if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    //     };
-    //     return null;
-    // };
-    // Usage example
-    // const token = JSON.parse(getCookie('cookieToken')); // Replace 'authToken' with the name of your cookie
-    // console.log('Token from cookie:', token);
-
     const location = useLocation(); // Hook to get the current location object
     useEffect(() => {
         const queryParams = new URLSearchParams(location.search);
