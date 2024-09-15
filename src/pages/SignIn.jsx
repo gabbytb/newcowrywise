51b88db30cb220f019b00049fba82ef506683241
gabbytb <igabriel.oyebanji@travelbeta.com>
2024-09-15 13:01:13 +0100

    Pushed to Repository (vybezonvibes@protonmail.com)

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import { useGoogleLogin } from '@react-oauth/google';
 import api from '../api';
 import axios from "axios";
